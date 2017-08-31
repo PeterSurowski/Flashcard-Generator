09d666b5324b28431a5597696e6816371c96099e
Peter Surowski <Peter Surowski>
2017-08-31 14:55:09 -0700

    All methods working.

--- ClozeCard.js
+++ ClozeCard.js
@@ -10,19 +10,24 @@ function ClozeCard(text, cloze) {
         var splitCloze = this.cloze.split(' ');
         //console.log(splitCloze);
         for (i = 0; i < splitCloze.length; i++) {
-            for (j = 0; j <splitCloze.length; j++) {
-                if (splitCloze[j] === splitString[i]) {
+            for (j = 0; j < splitString.length; j++) {
+                if (splitCloze[i] === splitString[j]) {
                     splitString.splice(i, 1);
                     splitString.splice(i, 0, '...');
-                }
-            }             
+                    //I could not figure out how to see whether none of the words matched the close, but I believe I would put that code here.                 
+                } 
+            }           
         }
         splitString = splitString.join(' ');
         console.log(splitString);
     };
-    this.fullText = function(){};
+    this.fullText = function(){
+        console.log(this.text);
+    };
 }
 
-var firstPresidentCloze = new ClozeCard("George Washington was the first president of the U.S.A.", "George Washington");
+var firstPresidentCloze = new ClozeCard("George Washington was the first president of the U.S.A.", "Carrot Top");
 
-firstPresidentCloze.partial()
+console.log(firstPresidentCloze.cloze);
+firstPresidentCloze.partial();
+firstPresidentCloze.fullText();
